c488d112581dff77e563ed8450ec6bcd8070e4fe
gadzhief <gadzhief@gmail.con>
2023-03-05 16:38:52 +0300

    Naming

--- src/main/java/com/projectx/crudapp/UserService.java
+++ src/main/java/com/projectx/crudapp/UserService.java
@@ -1,6 +1,5 @@
 package com.projectx.crudapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +7,12 @@ import java.util.List;
 @Service
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User saveUser(User user) {
         return userRepository.save(user);
